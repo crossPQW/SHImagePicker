ec052414370b1bbfb9eb1e72b642c69374653a71
huangshaohua <huangshaohua@xywy.com>
2015-08-03 17:25:59 +0800

    SHImagePicker
    模仿iOS系统自带iMessage发送图片的选择器

---
+++ SHImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHImagePicker
+//
+//  Created by 黄少华 on 15/8/3.
+//  Copyright (c) 2015年 黄少华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
